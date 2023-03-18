1f570d5fc041339b8be95f57d4c876459b471fa2
AndreSantos03 <93008364+AndreSantos03@users.noreply.github.com>
2023-03-18 21:47:44 +0000

    ITS WORKING HTTP GET

--- app/lib/remoteService.dart
+++ app/lib/remoteService.dart
@@ -4,24 +4,23 @@ import 'models/job.dart';
 import 'package:http/http.dart' as http;
 
 class RemoteService{
+
+  String authority = 'api.itjobs.pt';
+
   // Future<List<Job>?> getPosts() async{
   void getPosts() async{
-    var client = http.Client();
 
-    final response = await http.get
-      (
-        Uri.parse("https://api.itjobs.pt/job/list.json"),
-        headers:
-        {
-          'api_key': '3b579078bfd604421212c16bc7729440',
-        }
-    );
+    final queryParameters = {
+      'api_key' : '3b579078bfd604421212c16bc7729440',
+    };
+
+    final response = await http.get(Uri.https(authority,'job/list.json', queryParameters));
 
     if(response.statusCode == 200){
       //200 is the OK response code
       dynamic json = jsonDecode(response.body);
-      for(int i = 0; json['results'].length;i++){
-        
+      for(var i = 0; json['results'].length;i++){
+        print('yey');
       }
     }
     else{
