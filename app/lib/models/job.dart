ae431396f159b23b0ed8c9b64683316eb102e3c8
AndreSantos03 <andrebernardosantos@gmail.com>
2023-03-20 16:51:38 +0000

    incompleted, vai dar errros

--- app/lib/models/job.dart
+++ app/lib/models/job.dart
@@ -28,4 +28,40 @@ class Job{
     this.slug,
     this.company,
   });
+
+  factory Job.fromJson(dynamic json){
+    Map<String, dynamic> jsonCompany = json['company'];
+
+    Company jobCompany = Company(
+      id: jsonCompany['id'],
+      name: jsonCompany['name'],
+      logo: jsonCompany['body'],
+      description: jsonCompany['description'],
+      phone: jsonCompany['phone'],
+      email: jsonCompany['email'],
+      url: jsonCompany['url'],
+      slug: jsonCompany['slug'],
+    );
+
+    return Job(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      ref: json['ref'],
+      wage: json['wage'],
+      typeId: json['typeId'],
+      locationId: json['locationId'],
+      allowRemote: json['allowRemote'],
+      publishedAt: json['publishedAt'],
+      updatedAt: json['updatedAt'],
+      slug: json['slug'],
+      company: jobCompany,
+    );
+  }
+
+  static List<Job> jobsFromSnapshot(List snapshot){
+    return snapshot.map((data){
+      return Job.fromJson(data);
+    }).toList();
+  }
 }
