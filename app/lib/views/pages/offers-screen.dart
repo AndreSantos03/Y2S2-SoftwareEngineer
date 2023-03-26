e801e751276af7daef8a1d9da0559b762a19dca2
antoniorama <antonio.rama.03@gmail.com>
2023-03-26 15:16:27 +0100

    Company name added to the offers screen

--- app/lib/views/pages/offers-screen.dart
+++ app/lib/views/pages/offers-screen.dart
@@ -61,32 +61,48 @@ class _OffersScreenState extends State<OffersScreen> {
               ? Positioned(
                   left: screenWidth / 2 - screenWidth * 0.4,
                   // this last part should be half of the value we multiply in the width so it is centered
-                  top: screenHeight * 0.24,
+                  top: screenHeight * 0.26,
                   height: screenHeight * 0.60,
                   width: screenWidth * 0.8,
-                  child: ListView.builder(
-                    itemCount: jobs?.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: const EdgeInsets.all(25.0), // padding
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25.0),
-                          color: const Color.fromRGBO(255, 255, 255, 0.83),
-                        ),
-                        margin: const EdgeInsets.symmetric(vertical: 4.0),
-                        child: Text(
-                          jobs![index].title ?? '',
-                          style: SafeGoogleFont(
-                            'Poppins',
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
-                            height: 1.5,
-                            color: const Color(0x93050505),
-                          ),
-                        ),
-                      );
-                    },
+            child: ListView.builder(
+              itemCount: jobs?.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  padding: const EdgeInsets.all(25.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25.0),
+                    color: const Color.fromRGBO(255, 255, 255, 0.83),
                   ),
+                  margin: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        jobs![index].title ?? '',
+                        style: SafeGoogleFont(
+                          'Poppins',
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          height: 1.5,
+                          color: const Color(0x93050505),
+                        ),
+                      ),
+                      const SizedBox(height: 5), // add some space between the two Text widgets
+                      Text(
+                        jobs![index].company?.name ?? '',
+                        style: SafeGoogleFont(
+                          'Poppins',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          height: 1.5,
+                          color: const Color(0x93050505),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
                 )
               : const Center(child: CircularProgressIndicator()),
           // row in the bottom of the screen (buttons)
