75b606ec96b56cc65e095c73069bb2435ffc5d05
antoniorama <antonio.rama.03@gmail.com>
2023-04-02 19:52:30 +0100

    front-end changes (NavBar) and organized widgets
    New NavBar (collors to be decided later). Organized the program with NavBar widget and GradientBackGround widget.

--- app/lib/views/pages/map-screen.dart
+++ app/lib/views/pages/map-screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/views/pages/offers-screen.dart';
-import 'package:myapp/views/pages/user-screen.dart';
+import 'package:myapp/views/widgets/gradientBackground.dart';
+import 'package:myapp/views/widgets/navBar.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -15,149 +16,70 @@ class _MapScreenState extends State<MapScreen> {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
-    const double filter_settings_x_offset = 60;
-    const double filter_settings_y_offset = 40;
     return Scaffold(
-      body: Stack(
-        children: [
-          // Gradient background
-          Container(
-            height: screenHeight,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0, -7),
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromRGBO(225, 20, 20, 0.2),
-                  Color.fromRGBO(99, 138, 236, 0.69),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0, // keeps centered
-            right: 0, // keeps centered
-            bottom: 20,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // button_offers
-                TextButton(
-                  onPressed: () {
-                    // Changes to the other screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const OffersScreen()),
-                    );
-                  },
-                  child: SizedBox(
-                    width: screenWidth * 0.23,
-                    height: screenWidth * 0.23,
-                    child: Image.asset(
-                      'assets/page-1/images/buttonoffers.png',
-                      width: screenWidth * 0.23,
-                      height: screenWidth * 0.23,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 70), // add spacing between images
-                // button_map
-                SizedBox(
-                  width: screenWidth * 0.12,
-                  height: screenWidth * 0.12,
-                  child: Image.asset(
-                    'assets/page-1/images/buttonmap.png',
-                    width: screenWidth * 0.12,
-                    height: screenWidth * 0.12,
-                  ),
-                ),
-                const SizedBox(width: 70), // add spacing between images
-                // button_user
-                TextButton(
-                  onPressed: () {
-                    // Changes to the other screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UserScreen()),
-                    );
-                  },
-                  child: SizedBox(
-                    width: screenWidth * 0.155,
-                    height: screenWidth * 0.155,
-                    child: Image.asset(
-                      'assets/page-1/images/buttonuser.png',
-                      width: screenWidth * 0.155,
-                      height: screenWidth * 0.155,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // row de filtro e settings
-          Positioned(
-            left: 0,
-            right: 0,
-            top: screenHeight / 12,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      // Changes to the other screen
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const OffersScreen()),
-                      );
-                    },
-                    child: SizedBox(
-                      width: screenWidth * 0.10,
-                      height: screenWidth * 0.10,
-                      child: Image.asset(
-                        'assets/page-1/images/buttonfilter.png',
-                        width: screenWidth * 0.10,
-                        height: screenWidth * 0.10,
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: screenWidth - screenWidth / 2.5),
-              TextButton(
-                onPressed: () {
-                  // Changes to the other screen
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const OffersScreen()),
-                  );
-                },
-                child: SizedBox(
+      bottomNavigationBar: const NavBar(
+        pageNumber: 1,
+      ),
+      body: Stack(children: [
+        // Gradient background
+        GradientBackground(screenHeight: screenHeight),
+        // row de filtro e settings
+        Positioned(
+          left: 0,
+          right: 0,
+          top: screenHeight / 12,
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextButton(
+              onPressed: () {
+                // Changes to the other screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OffersScreen()),
+                );
+              },
+              child: SizedBox(
+                width: screenWidth * 0.10,
+                height: screenWidth * 0.10,
+                child: Image.asset(
+                  'assets/page-1/images/buttonfilter.png',
                   width: screenWidth * 0.10,
                   height: screenWidth * 0.10,
-                  child: Image.asset(
-                    'assets/page-1/images/buttonsettings.png',
-                    width: screenWidth * 0.10,
-                    height: screenWidth * 0.10,
-                  ),
                 ),
               ),
-            ]),
-          ),
-          Positioned(
-            left : 0,
-            right : 0,
-            top: screenHeight / 4,
-            child: SizedBox(
+            ),
+            SizedBox(width: screenWidth - screenWidth / 2.5),
+            TextButton(
+              onPressed: () {
+                // Changes to the other screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const OffersScreen()),
+                );
+              },
+              child: SizedBox(
+                width: screenWidth * 0.10,
+                height: screenWidth * 0.10,
                 child: Image.asset(
-                  'assets/page-1/images/imagemap.png',
-                  width: screenWidth,
-                  height: screenWidth,
-                )),
-          )
-        ]
-      ),
+                  'assets/page-1/images/buttonsettings.png',
+                  width: screenWidth * 0.10,
+                  height: screenWidth * 0.10,
+                ),
+              ),
+            ),
+          ]),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          top: screenHeight / 4,
+          child: SizedBox(
+              child: Image.asset(
+            'assets/page-1/images/imagemap.png',
+            width: screenWidth,
+            height: screenWidth,
+          )),
+        )
+      ]),
       backgroundColor: Colors.black,
     );
   }
--- app/lib/views/pages/offers-screen.dart
+++ app/lib/views/pages/offers-screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/utils.dart';
 import 'package:myapp/models/job.dart';
 import 'package:myapp/remoteService.dart';
-import 'package:myapp/views/pages/map-screen.dart';
-import 'package:myapp/views/pages/user-screen.dart';
+import 'package:myapp/utils.dart';
+import 'package:myapp/views/widgets/gradientBackground.dart';
+import 'package:myapp/views/widgets/navBar.dart';
 
 class OffersScreen extends StatefulWidget {
   const OffersScreen({Key? key}) : super(key: key);
@@ -40,22 +40,13 @@ class _OffersScreenState extends State<OffersScreen> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
+      bottomNavigationBar: const NavBar(
+        pageNumber: 0,
+      ),
       body: Stack(
         children: [
-          // Gradient background
-          Container(
-            height: screenHeight,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0, -7),
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromRGBO(225, 20, 20, 0.2),
-                  Color.fromRGBO(99, 138, 236, 0.69),
-                ],
-              ),
-            ),
-          ),
+          // Grading background
+          GradientBackground(screenHeight: screenHeight),
           isLoaded
               // scroll jobs
               ? Positioned(
@@ -64,115 +55,53 @@ class _OffersScreenState extends State<OffersScreen> {
                   top: screenHeight * 0.26,
                   height: screenHeight * 0.60,
                   width: screenWidth * 0.8,
-            child: ListView.builder(
-              itemCount: jobs?.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(25.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25.0),
-                    color: const Color.fromRGBO(255, 255, 255, 0.83),
-                  ),
-                  margin: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        jobs![index].title ?? '',
-                        style: SafeGoogleFont(
-                          'Poppins',
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                          height: 1.5,
-                          color: const Color(0x93050505),
+                  child: ListView.builder(
+                    itemCount: jobs?.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.all(25.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25.0),
+                          color: const Color.fromRGBO(255, 255, 255, 0.83),
                         ),
-                      ),
-                      const SizedBox(height: 5), // add some space between the two Text widgets
-                      Text(
-                        jobs![index].company?.name ?? '',
-                        style: SafeGoogleFont(
-                          'Poppins',
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          height: 1.5,
-                          color: const Color(0x93050505),
+                        margin: const EdgeInsets.symmetric(vertical: 4.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              jobs![index].title ?? '',
+                              style: SafeGoogleFont(
+                                'Poppins',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                                height: 1.5,
+                                color: const Color(0x93050505),
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            // add some space between the two Text widgets
+                            Text(
+                              jobs![index].company?.name ?? '',
+                              style: SafeGoogleFont(
+                                'Poppins',
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                height: 1.5,
+                                color: const Color(0x93050505),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                      );
+                    },
                   ),
-                );
-              },
-            ),
                 )
               : const Center(child: CircularProgressIndicator()),
-          // row in the bottom of the screen (buttons)
-          Positioned(
-            left: 0, // keeps centered
-            right: 0, // keeps centered
-            bottom: 20,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // button_offers
-                SizedBox(
-                  width: screenWidth * 0.23,
-                  height: screenWidth * 0.23,
-                  child: Image.asset(
-                    'assets/page-1/images/buttonoffers.png',
-                    width: screenWidth * 0.23,
-                    height: screenWidth * 0.23,
-                  ),
-                ),
-                const SizedBox(width: 70), // add spacing between images
-                // button_map
-                TextButton(
-                  onPressed: () {
-                    // Changes to the other screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const MapScreen()),
-                    );
-                  },
-                  child: SizedBox(
-                    width: screenWidth * 0.12,
-                    height: screenWidth * 0.12,
-                    child: Image.asset(
-                      'assets/page-1/images/buttonmap.png',
-                      width: screenWidth * 0.12,
-                      height: screenWidth * 0.12,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 70), // add spacing between images
-                // button_user
-                TextButton(
-                  onPressed: () {
-                    // Changes to the other screen
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const UserScreen()),
-                    );
-                  },
-                  child: SizedBox(
-                    width: screenWidth * 0.155,
-                    height: screenWidth * 0.155,
-                    child: Image.asset(
-                      'assets/page-1/images/buttonuser.png',
-                      width: screenWidth * 0.155,
-                      height: screenWidth * 0.155,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // text
+          // Text
           Positioned(
             top: screenHeight * 0.12,
-            left: 0, // keeps centered
-            right: 0, // keeps centered
+            left: 0,
+            right: 0,
             child: Text(
               'Aqui podes encontrar todas as\npropostas de emprego!\nClica em qualquer uma para\nsaber mais.',
               textAlign: TextAlign.center,
